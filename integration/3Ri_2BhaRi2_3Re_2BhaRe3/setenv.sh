7e1a670ee2513af603bfb423e7e5c1251a85b7d5
Fernando Giorgetti <fgiorget@redhat.com>
2019-01-21 13:53:53 -0200

    UPD(EdgeRouterMode02) - Added internal ansible roles and renamed edge router topology to match new convention defined internally by QE team

---
+++ integration/3Ri_2BhaRi2_3Re_2BhaRe3/setenv.sh
@@ -0,0 +1,8 @@
+export Router_I1=10.8.247.198
+export Router_I2=10.8.252.255
+export Router_I3=10.8.254.213
+export Router_E1=10.8.254.206
+export Router_E2=10.8.254.116
+export Router_E3=10.8.247.125
+export docker_host=10.8.250.140
+ROUTERS=($Router_I1 $Router_I2 $Router_I3 $Router_E1 $Router_E2 $Router_E3)
--- qpid-dispatch/EdgeRouter02/configs/broker-interior/generate_cfg.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-amqcfg -t artemis/2.6.0 -p artemis/2.6.0/ha/replication.yaml --tune tuning.slave -o slave/
-amqcfg -t artemis/2.6.0 -p artemis/2.6.0/ha/replication.yaml --tune tuning.master -o master/
-
-for instance in master slave; do
-    sed -i -e '/<\/addresses>/r broker_addresses.xml' -e 's/<\/addresses>//g' ${instance}/broker.xml
-done
